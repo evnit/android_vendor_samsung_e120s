f00becb9d90f6d143f8b64db9020d794beb6a702
evnit <evnithn@gmail.com>
2014-10-16 16:03:55 +0700

    Fix 2 flick screen

--- e120s-vendor-blobs.mk
+++ e120s-vendor-blobs.mk
@@ -83,7 +83,7 @@ PRODUCT_COPY_FILES += \
 	vendor/samsung/e120s/proprietary/lib/libloc_api-rpc-qc.so:system/lib/libloc_api-rpc-qc.so \
 	vendor/samsung/e120s/proprietary/lib/libcommondefs.so:system/lib/libcommondefs.so \
 	vendor/samsung/e120s/proprietary/lib/librpc.so:system/lib/librpc.so \
-	vendor/samsung/e120s/proprietary/lib/hw/sensors.MSM8660_SURF.so:system/lib/hw/sensors.MSM8660_SURF.so \
+	vendor/samsung/e120s/proprietary/lib/hw/sensors.default.so:system/lib/hw/sensors.default.so \
 	vendor/samsung/e120s/proprietary/lib/libakm.so:system/lib/libakm.so \
 	vendor/samsung/e120s/proprietary/lib/hw/camera.vendor.msm8660.so:system/lib/hw/camera.vendor.msm8660.so \
 	vendor/samsung/e120s/proprietary/lib/libmmjpeg.so:system/lib/libmmjpeg.so \
